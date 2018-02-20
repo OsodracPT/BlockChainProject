1f76c14450c9dea74f07668e0e1b327b753ca523
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-02-20 17:51:57 +0000

    Testing for valid blockchain

--- main.js
+++ main.js
@@ -32,10 +32,26 @@ class Blockchain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.lenght; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 let osodracCoin = new Blockchain();
 osodracCoin.addBlock(new Block(1,"30/01/2018", {amount:4}));
 osodracCoin.addBlock(new Block(1,"25/04/2018", {amount:10}));
 
-console.log(JSON.stringify(osodracCoin, null, 4));
+console.log('Is the BlockChain Valid?'+ osodracCoin.isChainValid());
