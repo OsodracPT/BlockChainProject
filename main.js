74a59d380c14cb8567da4030bdcce4900fc49103
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-02-22 12:30:24 +0000

    Implementing Proof-of-Work
    Created a mining method in javascript.

--- main.js
+++ main.js
@@ -7,16 +7,27 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash=this.calculateHash();
+        this.nonce=0;
     }
 
     calculateHash(){
-return SHA256(this.index +this.previousHash+this.timestamp+ JSON.stringify(this.data)).toString();
+return SHA256(this.index +this.previousHash+this.timestamp+ JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Bock mined:" + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty=2;
     }
 
     createGenesisBlock(){
@@ -29,7 +40,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -52,11 +63,9 @@ class Blockchain{
 }
 
 let osodracCoin = new Blockchain();
+
+console.log("Mining block 1...");
 osodracCoin.addBlock(new Block(1,"30/01/2018", {amount:4}));
+
+console.log("Mining block 2...");
 osodracCoin.addBlock(new Block(2,"25/04/2018", {amount:10}));
-
-console.log('Is the BlockChain Valid?'+ osodracCoin.isChainValid());
-
-osodracCoin.chain[1].data = { amount: 100 };
-osodracCoin.chain[1].hash = osodracCoin.chain[1].calculateHash();
-console.log('Is the BlockChain Valid?'+ osodracCoin.isChainValid());
